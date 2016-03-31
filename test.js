578d1b08da58d45f7020c0edc04497ec9d188ae2
ductranvip95 <ductranvip95@gmail.com>
2016-03-31 17:01:23 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 var http = require("http");
-
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
-}).listen(OPENSHIFT_NODEJS_PORT);
+}).listen(server_port, server_ip_address);
